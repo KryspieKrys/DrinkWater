30694347586ab495a0b6fefe4ee32f3abf0c47a7
KryspieKrys <christ.djie@gmail.com>
2026-05-12 23:25:18 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Memungkinkan app di-install ke Home Screen HP
 // ============================================================
 
-const CACHE_NAME = "hydrotrack-v1";
+const CACHE_NAME = "hydrotrack-v2";
 const ASSETS_TO_CACHE = [
   "./user.html",
   "./user.js",
